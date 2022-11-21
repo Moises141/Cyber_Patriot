c6b3a25b3d0174f8841094fd30872e96b86b8ab9
UnformedHail148 <91559179+Moises141@users.noreply.github.com>
2022-11-21 08:41:51 -0800

    Delete Project directory

--- Project/RemoveUser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-echo "You have selected Option Two"
-sleep 3
-clear
-echo "what is name of the user you would like to Remove?"
-read input
-sudo userdel $input
-echo "The User "$input "has Successfully Removed!"
